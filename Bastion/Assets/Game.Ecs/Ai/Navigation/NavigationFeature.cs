2da6348506750044cd383c176b0ad406ff8ac676
Roman <avgustin4ik68@gmail.com>
2024-03-22 22:20:17 +0700

    +remove comments

--- Bastion/Assets/Game.Ecs/Ai/Navigation/NavigationFeature.cs
+++ Bastion/Assets/Game.Ecs/Ai/Navigation/NavigationFeature.cs
@@ -1,12 +1,9 @@
 namespace Game.Ecs.Ai.Navigation
 {
     using Cysharp.Threading.Tasks;
-    using Events;
     using Leopotam.EcsLite;
-    using Leopotam.EcsLite.ExtendedSystems;
     using Systems;
     using UniGame.LeoEcs.Bootstrap.Runtime;
-    using Unity.Mathematics;
     using UnityEngine;
 
     /// <summary>
@@ -15,16 +12,8 @@ namespace Game.Ecs.Ai.Navigation
     [CreateAssetMenu(menuName = "Game/Feature/Gameplay/NavigationFeature")]
     public class NavigationFeature : BaseLeoEcsFeature
     {
-        public Transform testEndPositionTransform;
         public override async UniTask InitializeFeatureAsync(IEcsSystems ecsSystems)
         {
-            float3 destination = testEndPositionTransform.position;
-            ecsSystems.DelHere<DestinationIsReachedEvent>();
-            //check distance by moving to target system
-            // ecsSystems.Add(new MovingToTargetSystem());
-
-            //set destination for all agents
-            // ecsSystems.Add(new SetDestinationTestSystem(destination));
             ecsSystems.Add(new SetDestinationToEnemySystem());
 
         }
