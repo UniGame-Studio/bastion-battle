00fcd38bfca7a79b5820fb6737a33ee214138553
Roman <avgustin4ik68@gmail.com>
2024-03-26 22:30:41 +0700

    +ability feature +damage feature in progress

--- Bastion/Assets/Game.Ecs/Ai/AiFeature.cs
+++ Bastion/Assets/Game.Ecs/Ai/AiFeature.cs
@@ -2,17 +2,20 @@ namespace Game.Ecs.Ai
 {
     using Cysharp.Threading.Tasks;
     using Leopotam.EcsLite;
+    using Systems;
     using UniGame.LeoEcs.Bootstrap.Runtime.Config;
     using UnityEngine;
 
     /// <summary>
     /// Group of features for gameplay units
     /// </summary>
-    [CreateAssetMenu(menuName = "Game/Feature/Gameplay/UnitFeature", fileName = "Unit Feature")]
+    [CreateAssetMenu(menuName = "Game/Feature/Gameplay/UnitAiFeature", fileName = "Unit Ai Feature")]
     public class AiFeature : LeoEcsFeatureGroupAsset
     {
         protected override UniTask OnPostInitializeFeatureAsync(IEcsSystems ecsSystems)
         {
+            //test
+            ecsSystems.Add(new TakeDamageByCooldownSystem());
             return base.OnPostInitializeFeatureAsync(ecsSystems);
         }
     }
